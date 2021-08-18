2afd0f0bc9866c0c5703f51767f765fb6d9806f6
Ondorion <maks.pannf@gmail.com>
2021-08-18 18:55:38 +0300

    added product-2

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
 $(function(){
+
+  $('.filter-prise__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-prise__from').text(data.from);
+      $('.filter-prise__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-prise__from').text(data.from);
+      $('.filter-prise__to').text(data.to);
+    },
+  })
+
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -6,6 +21,14 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 2000
   });
+
+  $('.star').rateYo({
+    readOnly: true,
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    spacing: "7px"
+  });
+  
 
 
   var mixer = mixitup('.product-info__content');
