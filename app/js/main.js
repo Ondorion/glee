c21e8d4f77323cd14bb292e99cb46ca308a898ba
Ondorion <maks.pannf@gmail.com>
2021-08-24 18:20:42 +0300

    added product-details page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,30 @@
-$(function(){
+$(function () {
+
+
+  $('.details-slider__inner').slick({
+    slidesToShow: 4,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.png" alt=""></button>'
+
+  });
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
+
+
 
   $('.filter-prise__input').ionRangeSlider({
     type: "double",
@@ -22,13 +48,22 @@ $(function(){
     autoplaySpeed: 2000
   });
 
+  $('.product-item__star').rateYo({
+    readOnly: true,
+    starWidth: "17px",
+    normalFill: "#d6d6d6",
+    spacing: "7px"
+  });
+
   $('.star').rateYo({
     readOnly: true,
     starWidth: "11px",
     normalFill: "#d6d6d6",
     spacing: "7px"
   });
-  
+
+  $('.product-item__num').styler();
+
 
 
   var mixer = mixitup('.product-info__content');
